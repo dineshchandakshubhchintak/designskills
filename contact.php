5bf8a7dd40d2dd48f81e3705cbf72eb7d8aa049a
Chinmay Bhat <0310chinmay@gmail.com>
2023-03-24 17:04:37 +0530

    updated tags

--- contact.php
+++ contact.php
@@ -34,11 +34,6 @@
 <meta property="twitter:description" content="Need help, have feedback or a question? Get in touch with our team via our contact form or through our email, phone number or social media accounts. We're here to help and value your feedback">
 <meta property="twitter:image" content="https://designskills.com/img/logo/Design_Skills_Academy_Logo.png">
 
-<!-- Page Title -->
-
-<title>Designskill</title>
-
-
 
 <!-- Favicon and Touch Icons -->
 
